d219a4e82bf8493977ee27a8e37230270f6411a8
Channelal2001 <alfredo.alexander@students.salle.url.edu>
2023-05-09 18:51:20 +0200

    Token divisor

---
+++ src/components/divideToken.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    methods: {
+        divideToken(token) {
+            const [header, payload, signature] = token.split('.');
+            const extractedPayload = JSON.parse(atob(payload));
+            const {id} = extractedPayload;
+            return id;
+        }
+    }
+}
+</script>
