323eb8dc8856acbbe8a5ed6fdae0ffb0bbc44f04
Channelal2001 <125389432+Channelal2001@users.noreply.github.com>
2023-05-10 11:26:10 +0200

    Update signup.vue

--- src/views/signup.vue
+++ src/views/signup.vue
@@ -28,7 +28,7 @@ export default {
             })
             .then((response) => {
                 if (response.status === 201) {
-                    window.location.href = "/";
+                    window.location.href = "/login";
                     return response.json();
                 } else {
                     switch(response.status) {
