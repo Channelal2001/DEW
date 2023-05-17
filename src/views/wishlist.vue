bf4dc359a36e578756305fa61510806ed028d793
Neptune-Life <77457951+Neptune-Life@users.noreply.github.com>
2023-05-17 15:55:16 +0200

    upload

--- src/views/wishlist.vue
+++ src/views/wishlist.vue
@@ -87,23 +87,17 @@ export default {
     },
     hideWhislistInfo() {
       var x = document.getElementById("wishlist-data-information");
+        var y = document.getElementById("hide-wishlist-data");
 
       if (x.style.display === "none") {
           x.style.display = "block";
+          y.style.display = "none";
       } else {
           x.style.display = "none";
+          y.style.display = "block";
       }
     },
-    showWhislistInfo() {
-      var x = document.getElementById("hide-wishlist-data");
-
-      if (x.style.display === "none") {
-          x.style.display = "block";
-        
-      } else {
-          x.style.display = "none";
-      }
-    },
+    
     hideFirstGiftMessage(numberOfItems) {
         var x = document.getElementById("message-empty");
         if (numberOfItems > 0) {
@@ -223,28 +217,37 @@ export default {
                     </div>
                 </div>
                 <div id="hide-wishlist-data">
-                <div id="wishlist-information">
-                <div class="celebration-date-box">
-                    <p id="text-celebration-box">Celebration day in</p>
-                    <p id="information-wishlist-numbers-box">10 days 2h 11 min</p>
-                </div>
+                        <div id="wishlist-information">
+                    <div class="celebration-date-box">
+                        <p id="text-celebration-box">Celebration day in</p>
+                        <p id="information-wishlist-numbers-box">10 days 2h 11 min</p>
+                    </div>
 
-                <div id="wishlist-data-information">
-                    <div>
-                        <div class="image-name-new-gift">
-                            <svg href="#" class="image-icon-wishlist" viewBox="0 0 20 20">
-                            <path d="M18.555,15.354V4.592c0-0.248-0.202-0.451-0.45-0.451H1.888c-0.248,0-0.451,0.203-0.451,0.451v10.808c0,0.559,0.751,0.451,0.451,0.451h16.217h0.005C18.793,15.851,18.478,14.814,18.555,15.354 M2.8,14.949l4.944-6.464l4.144,5.419c0.003,0.003,0.003,0.003,0.003,0.005l0.797,1.04H2.8z M13.822,14.949l-1.006-1.317l1.689-2.218l2.688,3.535H13.822z M17.654,14.064l-2.791-3.666c-0.181-0.237-0.535-0.237-0.716,0l-1.899,2.493l-4.146-5.42c-0.18-0.237-0.536-0.237-0.716,0l-5.047,6.598V5.042h15.316V14.064z M12.474,6.393c-0.869,0-1.577,0.707-1.577,1.576s0.708,1.576,1.577,1.576s1.577-0.707,1.577-1.576S13.343,6.393,12.474,6.393 M12.474,8.645c-0.371,0-0.676-0.304-0.676-0.676s0.305-0.676,0.676-0.676c0.372,0,0.676,0.304,0.676,0.676S12.846,8.645,12.474,8.645"></path>
-                            </svg>
-                            <p id="name-new-gift">iPhone 14 Pro ultra</p>
+                    <div id="wishlist-data-information">
+                        <div class="buttons-navegation-new-gift">
+                            <a href="/wishlist">
+                                <svg id="svg-icon-cross" viewBox="0 0 20 20">
+                                    <path  d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
+                                </svg>
+                            </a>
+                            <a href="/wishlist" id="button-add">Add</a>
                         </div>
-                        <p id="description-neuw-gift">Description product Description product Description product Description product Description product</p>
-                        <p id="text-link-gift">Website to find it</p>
+                        <div>
+                            <div class="image-name-new-gift">
+                                
+                                <p id="name-new-gift">Name of the gift</p>
+                            </div>
+                            <p id="description-neuw-gift">Description product Description product Description product Description product Description product</p>
+                        </div>
+                        <div class="buttons-source">
+                            <p>Add to find it:</p>
+                            <div class="buttons-source-gift">
+                                <p href="#" id="button-add-link">Add link</p>
+                                <p href="#" id="button-mercado-expres">Mercado expres</p>
+                                <p href="#" id="button-amazon">Amazon</p>
+                            </div>
+                        </div>
                     </div>
-                    <div id="navegation-gift">
-                        <a href="/wishlistMoving" id="button-move">Move</a>
-                        <a href="" id="button-delete">Delete</a>
-                    </div>
-                </div>
                 </div>
             </div>
         </div>
