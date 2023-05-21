744b4277cc552f10de286812ff97ff2f6efb4543
Neptune-Life <77457951+Neptune-Life@users.noreply.github.com>
2023-05-21 18:30:12 +0200

    upl

--- src/views/chat.vue
+++ src/views/chat.vue
@@ -27,7 +27,7 @@
         <section class="chats-dashboard">
             <div id="chat-open">
                 <div id="search-bar">
-                        <input id="search-input" type="text" name="search" placeholder="Search..">
+                        <input id="search-input" type="text" name="search" placeholder="Search...">
                 </div>
                 <div class="messages-box">
                     <div class="chat-user">
@@ -57,14 +57,17 @@
                 </div>
             </div>
             <section class="chat-open">
+                <div class="chat-box">
                 <div id="name-user-chat">
                     <p id="user-name">Alessandro Sadany</p>
                 </div>
                 <div class="messages">
 
                 </div>
+                 </div>
                 <div id="text-imput">
-                    <p id="text-imput-chat">Type your message here...</p>
+                    <input id="text-imput-chat" type="text" name="search" placeholder="Type your message here...">
+                    
                     <div href="#" id="icon-send-back">
                         <svg id="icon-send " xmlns="http://www.w3.org/2000/svg " width="16 " height="16 " fill="currentColor " class="bi bi-send " viewBox="0 0 16 16 "> <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643
                         7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z " fill="white "></path> </svg>
