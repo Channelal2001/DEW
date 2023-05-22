a2cdd947dec79e9d0b0bfb6a40f9ecbf1925fa93
Neptune-Life <77457951+Neptune-Life@users.noreply.github.com>
2023-05-22 18:36:55 +0200

    upload

--- src/views/home.vue
+++ src/views/home.vue
@@ -487,7 +487,7 @@
               </div>
             </div>
           </div>
-          <section>
+         <!-- <section>
         <button class="unstyle filters" @click= "hidefilters" >
           <p>Filters</p>
           <svg
@@ -512,7 +512,7 @@
             <button class="unstyle" id="filters-type-3">1 month</button>
           </div>
         </div>
-      </section>
+      </section>-->
 
       <!-- Following-->
       <section>
@@ -521,16 +521,8 @@
         </div>
       </section>
 
-      <section id="slider">
-        <a href="">
-            <div id="icon-left">
-                <svg class="svg-icon" viewBox="0 0 20 20">
-                <path
-                    d="M11.739,13.962c-0.087,0.086-0.199,0.131-0.312,0.131c-0.112,0-0.226-0.045-0.312-0.131l-3.738-3.736c-0.173-0.173-0.173-0.454,0-0.626l3.559-3.562c0.173-0.175,0.454-0.173,0.626,0c0.173,0.172,0.173,0.451,0,0.624l-3.248,3.25l3.425,3.426C11.911,13.511,11.911,13.789,11.739,13.962 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.148,3.374,7.521,7.521,7.521C14.147,17.521,17.521,14.148,17.521,10"
-                ></path>
-                </svg>
-            </div>
-        </a>
+      <section id="list">
+        
         <div id="slider-wishlists">
           <article>
             <div class="present-cover">
@@ -546,15 +538,6 @@
             </div>
           </article>
         </div>
-        <div id="icon-right">
-          <a href="">
-            <svg class="svg-icon" viewBox="0 0 20 20">
-              <path
-                d="M12.522,10.4l-3.559,3.562c-0.172,0.173-0.451,0.176-0.625,0c-0.173-0.173-0.173-0.451,0-0.624l3.248-3.25L8.161,6.662c-0.173-0.173-0.173-0.452,0-0.624c0.172-0.175,0.451-0.175,0.624,0l3.738,3.736C12.695,9.947,12.695,10.228,12.522,10.4 M18.406,10c0,4.644-3.764,8.406-8.406,8.406c-4.644,0-8.406-3.763-8.406-8.406S5.356,1.594,10,1.594C14.643,1.594,18.406,5.356,18.406,10M17.521,10c0-4.148-3.374-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.147,17.521,17.521,14.147,17.521,10"
-              ></path>
-            </svg>
-          </a>
-        </div>
       </section>
 
       <!-- My Wishlists-->
@@ -564,7 +547,7 @@
         </div>
       </section>
       
-      <section id="lists">
+      <section id="list">
           <article v-for="myWishlist in myWishlists" :key="myWishlist.id">
             <div class="present-cover">
               <div class="text-present">
@@ -575,8 +558,8 @@
                   <p style="margin-bottom: 3px; margin-top: 5px"><b>Celebration day in:</b></p>
                   <p style="margin-top: 0"> {{ formatDate(myWishlist.end_date) }}</p>
                 </div>
-                <a href="" class="button_share">Share</a>
-                <button @click="editWishlist(myWishlist.id)" class="button_share">Edit</button>
+                <!--<a href="" class="button_share">Share</a>-->
+                <button @click="editWishlist(myWishlist.id)" class="button_edit">Edit</button>
               </div>
             </div>
           </article>
--- src/views/wishlist.vue
+++ src/views/wishlist.vue
@@ -97,6 +97,7 @@ export default {
           y.style.display = "block";
       }
     },
+   
     shoeWhislistInfo() {
       var x = document.getElementById("wishlist-data-information");
         var y = document.getElementById("hide-wishlist-data");
@@ -225,7 +226,9 @@ export default {
                     </div>
                     <a href="/wishlistGiftReserved" id="notifications">Reserve</a>
                 </div>
-                <p id="message-emty">Add the first gift</p>
+                <!--<div id="hide-text">-->
+                    <p id="message-emty">Add the first gift</p>
+               <!-- </div>-->
             </div>
             <div id="wishlist-information">
                 <div id="wishlist-data-information">
@@ -263,7 +266,7 @@ export default {
                                     <path  d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
                                 </svg>
                             </a>
-                            <a href="/wishlist" id="button-add">Add</a>
+                            <a @click="hidenothingGift" id="button-add">Add</a>
                         </div>
                         <div>
                             <div class="image-name-new-gift">
@@ -291,7 +294,7 @@ export default {
                     </div>
                 </div>
             </div>
-        </div>
+            </div>
             
         </section>
     </main>
