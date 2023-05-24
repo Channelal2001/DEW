03a7b89751ca56fd4c30a56f38d12ade6c24ca15
Neptune-Life <77457951+Neptune-Life@users.noreply.github.com>
2023-05-24 20:45:12 +0200

    Update wishlist.vue

--- src/views/wishlist.vue
+++ src/views/wishlist.vue
@@ -110,15 +110,7 @@ export default {
             y.style.display = "none";
         }
     },
-    
-    hideFirstGiftMessage(numberOfItems) {
-        var x = document.getElementById("message-empty");
-        if (numberOfItems > 0) {
-            x.display = "none";
-        } else {
-            x.display = "block";
-        }
-    },
+   
     showLink(){
         var x = document.getElementById("hide-link");
         if (x.style.display === "none") {
@@ -225,9 +217,9 @@ export default {
                     </div>
                     <a href="/wishlistGiftReserved" id="notifications">Reserve</a>
                 </div>
-                <!--<div id="hide-text">-->
+                <div id="hide-text">
                     <p id="message-emty">Add the first gift</p>
-               <!-- </div>-->
+               </div>
             </div>
             <div id="wishlist-information">
                 <div id="wishlist-data-information">
@@ -265,7 +257,9 @@ export default {
                                     <path  d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
                                 </svg>
                             </a>
-                            <a @click="hidenothingGift" id="button-add">Add</a>
+                            
+                            <a @click="hideFirstGiftMessage" id="button-add">Add</a>
+                           
                         </div>
                         <div>
                             <div class="image-name-new-gift">
