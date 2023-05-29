199b6b711e027c3f7406622f033ecf2688f4d634
Channelal2001 <125389432+Channelal2001@users.noreply.github.com>
2023-05-29 18:17:40 +0200

    Update home.vue

--- src/views/home.vue
+++ src/views/home.vue
@@ -537,7 +537,7 @@ export default {
                   <p>{{ friendWishlist.name }}</p>
                 </div>
                 <div class="text-time-present">
-                  <p style="margin-bottom: 3px; margin-top: 5px"><b>Celebration day in:</b></p>
+                  <p style="margin-bottom: 3px; margin-top: 5px"><b>Celebration day:</b></p>
                   <!-- TODO: Modificar la funció formatDate perquè retorni els dies que queden per què acabi la wishlist -->
                   <p style="margin-top: 0">{{ formatDate(friendWishlist.end_date) }}</p>
                 </div>
