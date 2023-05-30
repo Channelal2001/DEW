7c98dce475c48276051688b5eaf8dc7b17800787
Channelal2001 <125389432+Channelal2001@users.noreply.github.com>
2023-05-30 16:03:19 +0200

    Update forgotPassword.vue

--- src/views/forgotPassword.vue
+++ src/views/forgotPassword.vue
@@ -2,17 +2,59 @@
 export default {
   data() {
     return {
-      friends: [],
-      myWishlists: [],
-      friendsWishlists: [],
-      requests: [],
-      users: [],
-      usersWishlists: [],
     };
   },
   methods: {
+    changePassword(userData) {
+      const token = localStorage.getItem('token');
+      const name = userData.name;
+      const lastname = userData.last_name;
+      const email = userData.email;
+      const password = document.getElementById('password').value;
+      const image = document.getElementById('button-edit-image').value;
+      const user = {
+        name: name,
+        last_name: lastname,
+        email: email,
+        password: password,
+        image: image,
+      }
+      fetch('https://balandrau.salle.url.edu/i3/socialgift/api/v1/users', {
+        method: 'PUT',
+        headers: {
+          'accept': 'application/json',
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          alert('User edited');
+          window.location.href="javascript:history.back()";
+          return response.json();
+        } else {
+          switch(response.status) {
+            case 400:
+              alert('Bad request');
+              break;
+            case 401:
+              alert('Unauthorized');
+              break;
+            case 406:
+              alert('Missing parameters');
+              break;
+            case 409:
+              alert('The email adress has already been previously registered');
+              break;
+            case 410:
+              alert('No user has been edited');
+          }
+        }
+      })
+    },
     search() {
-      const searchTerm = document.getElementById("search-bar").value;
+      const searchTerm = document.getElementById("email").value;
       const token = localStorage.getItem('token');
       fetch(`https://balandrau.salle.url.edu/i3/socialgift/api/v1/users/search?s=${searchTerm}`, {
         method: 'GET',
@@ -44,12 +86,8 @@ export default {
           }
         }
       })
-      .then((usersData) => {
-        this.users = usersData;
-        this.users.forEach((user) => {
-          this.loadWishlists(user.id);
-        });
-        this.showSearch();
+      .then((userData) => {
+        this.changePassword(userData)
       })
       document.getElementById("search-bar").value = "";
     },
