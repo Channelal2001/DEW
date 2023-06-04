3f00471da768fd28a0c075d40155e0eb0479b6d4
Neptune-Life <77457951+Neptune-Life@users.noreply.github.com>
2023-06-04 19:49:15 +0200

    uplad

--- src/views/chat.vue
+++ src/views/chat.vue
@@ -80,6 +80,13 @@ export default {
         if (response.status === 200) {
           const mensaje = [];
           const messagesData = await response.json();
+          var x = document.getElementById("hide-messages");
+
+          if (x.style.display === "block") {
+            x.style.display = "none";
+          } else {
+            x.style.display = "block";
+          }
           for (const message of messagesData) {
             const username = await this.getUsername(message.user_id_send);
             mensaje.push({
@@ -319,28 +326,29 @@ export default {
                     </button>
                 </div>
             </div>
-            <section class="chat-open">
-                <div class="chat-box">
-                  <div id="name-user-chat">
-                      <p id="user-name"></p>
-                  </div>
-                  <div class="messages">
-                    <div v-for="message in messages" :key="message.id">
-                      <p id="message-text">{{ message.username }}: {{ message.content }}</p>
+            <div id="hide-messages">
+              <section class="chat-open">
+                  <div class="chat-box">
+                    <div id="name-user-chat">
+                        <p id="user-name"></p>
+                    </div>
+                    <div class="messages">
+                      <div v-for="message in messages" :key="message.id">
+                        <p id="message-text">{{ message.username }}: {{ message.content }}</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div id="text-input">
-                    <input id="text-input-chat" type="text" name="search" placeholder="Type your message here...">
-                    
-                    <div @click="sendMessage" id="icon-send-back">
-                        <svg id="icon-send " xmlns="http://www.w3.org/2000/svg" width="16 " height="16 " fill="currentColor " class="bi bi-send " viewBox="0 0 16 16 "> <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643
-                        7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z " fill="white "></path> </svg>
-                    </div>
-                </div>
-            </section>
+                  <div id="text-input">
+                      <input id="text-input-chat" type="text" name="search" placeholder="Type your message here...">
+                      
+                      <div @click="sendMessage" id="icon-send-back">
+                          <svg id="icon-send " xmlns="http://www.w3.org/2000/svg" width="16 " height="16 " fill="currentColor " class="bi bi-send " viewBox="0 0 16 16 "> <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643
+                          7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z " fill="white "></path> </svg>
+                      </div>
+                  </div>
+              </section>
+            </div>
         </section>
-
     </main>
     <footer></footer>
     </div>
